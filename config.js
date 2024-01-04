e1f2f5b05bf873068cf80328d3dc6b1f8d8f56d8
Hải Anh <nghanh762003.it@gmail.com>
2024-01-04 23:28:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em à!',
+    introTitle: 'Hế lu bà!',
     introDesc: `Trái đất vốn lạ thường.
-    Mà sao em đi nhầm đường.
-    Lạc vào tim anh lẻ loi.
+    Mà sao bạn đi nhầm đường.
+    Lạc vào tim tui lẻ loi.
     Đằng sau chữ yêu đây là thương <3`,
     btnIntro: 'hihi',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng tui đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng tui đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích tui <33',
     btnNo: 'Hong bé ơi :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích anh <3',
-    btnReply: 'Gửi cho anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích tui <3',
+    btnReply: 'Gửi cho tui <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰 Thích em nhiều nhiều 😘😘',
-    messDesc: 'Đi xem phim với anh nha thiên thần.',
+    mess: 'Tui biết mà 🥰 Thích bà nhiều nhiều 😘😘',
+    messDesc: 'Đi xem phim với tui nha thiên thần.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/nghanh762003'
 }
